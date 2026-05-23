ed9d600b8a32abdac10320c276ce40d5727e97e6
matheusghb <mistermquack124@gmail.com>
2026-05-23 00:02:13 -0300

    transferindo pra react native

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native' // importa os elementos da tela.
+const Home = () => { // cria uma função que será usada pra renderizar os elementos.
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+export default Home
+const styles = StyleSheet.create({
+
+})
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+> ## NEWCHESS 
+
+ Esse repositório apresenta um projeto direcionado a cadeira de **Código Mobile** da **Uninassau Paulista**, sobre supervisão do professor Josivan. O aplicativo foi feito dentro do *visual studio code* utilizando de código *react native* e back-end do *appwrite*.
